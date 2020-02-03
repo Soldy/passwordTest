8f78eefdebc2af1bf663f21f1ef4aa3f293a1171
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-03 17:23:04 +0000

    jump to es8
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,87 @@
 "use strict";
 
 exports.password=function(){
-    this.setup={
+    /*
+     * @public
+     * object
+     */
+    this.check=function(pass){
+        password = pass;
+        reset();
+        if(setup.check.upperCase)
+            log.checks.upperCase = checkCase(
+                password.match(/[A-Z]/g),
+                setup.min.upperCase,
+                setup.max.upperCase,
+                "upperCase"
+            );
+        if(setup.check.lowerCase)
+            log.checks.lowerCase = checkCase(
+                password.match(/[a-z]/g),
+                setup.min.lowerCase,
+                setup.max.lowerCase,
+                "lowerCase"
+            );
+        if(setup.check.number)
+            log.checks.number = checkCase(
+                password.match(/\d/g),
+                setup.min.number,
+                setup.max.number,
+                "number"
+            );
+        if(setup.check.special)
+            log.checks.special = checkCase(
+                password.match(/[$()@.!%*#?&]/g),
+                setup.min.special,
+                setup.max.special,
+                "special"
+            );
+        if(setup.check.size)
+           log.checks.size = checkCase(
+                password,
+                setup.min.size,
+                setup.max.size,
+                "size"
+            );
+        return log.result;
+
+    };
+    /* @param string {type}
+     * @param string {name}
+     * @param string/number {value}
+     * @public
+     * boolean
+     */
+    this.set=function(type, name, value){
+        if(typeof setup[type] === "undefined")
+            return false;
+        if(typeof setup[type][name] === "undefined")
+            return false;
+        if(
+            (type === "check")&&
+            (0 > [true, false].indexOf(value))
+        )
+            return false;
+        if(["min","max"].indexOf(type) > -1){
+            if (parseInt(value).toString() !== value.toString())
+                return false;
+            value = parseInt(value);
+        }
+        setup[type][name]=value;
+        return true;
+    };
+    /*
+     * @public
+     * object
+     */
+    this.getLog = function(){
+        return log;
+    };
+    /*
+     * @private 
+     * object 
+     */
+    let setup={
         check:{
             upperCase:true,
             lowerCase:true,
@@ -24,9 +104,14 @@ exports.password=function(){
             size:19,
         }
     };
-    this.log={};
-    this.reset=function(){
-        passt.log={
+    /*
+     * @private
+     * object
+     */
+    let log={};
+    let password="";
+    let reset=function(){
+        log={
             checks:{
                 min:{
                     upperCase:true,
@@ -48,114 +133,59 @@ exports.password=function(){
             result:true
         };
     };
-    this.password="";
-    this.set=function(type, name, value){
-        if(typeof passt.setup[type] === "undefined")
-            return false;
-        if(typeof passt.setup[type][name] === "undefined")
-            return false;
-        if(
-            (type === "check")&&
-            (0 > [true, false].indexOf(value))
-        )
-            return false;
-        if(["min","max"].indexOf(type) > -1){
-            if (parseInt(value).toString() !== value.toString())
-                return false;
-            value = parseInt(value);
+    /*
+     * @private
+     * boolean
+     */
+    let failed = function(target, limit){
+        if(typeof limit === "undefined"){
+            limit = "min";
+        }else{
+            limit = "max";
         }
-        passt.setup[type][name]=value;
+        if(
+            (typeof target !== "undefined")&&
+            (typeof log.checks[limit][target] !== "undefined")
+        )
+            log.checks[limit][target] = false;
+            
+        log.result=false;
+        return false;
+    };
+    /*
+     * @private
+     * boolean
+     */
+    let checkCase =function(checkStr, min, max, target){
+        if (
+            (typeof checkStr === "undefined")||
+            (checkStr === null)
+        )
+            return failed(target);
+        if(
+            (parseInt(min).toString() != min.toString())||
+            (parseInt(max).toString() != max.toString())
+        )
+            return failed(target);
+        if(
+            (min > 0) &&
+            (min > checkStr.length)
+        )
+            return failed(target);
+        if(
+            (max > 0) &&
+            (checkStr.length > max)
+        )
+            return failed(target, 1);
         return true;
     };
-    this.getLog = function(){
-        return passt.log;
-    };
-    this.tools={
-        failed:function(target, limit){
-            if(typeof limit === "undefined"){
-                limit = "min";
-            }else{
-                limit = "max";
-            }
-            if(
-                (typeof target !== "undefined")&&
-                (typeof passt.log.checks[limit][target] !== "undefined")
-            )
-                passt.log.checks[limit][target] = false;
-                
-            passt.log.result=false;
-            return false;
-        },
-        check:function(checkStr, min, max, target){
-            if (
-                (typeof checkStr === "undefined")||
-                (checkStr === null)
-            )
-                return passt.tools.failed(target);
-            if(
-                (parseInt(min).toString() != min.toString())||
-                (parseInt(max).toString() != max.toString())
-            )
-                return passt.tools.failed(target);
-            if(
-                (min > 0) &&
-                (min > checkStr.length)
-            )
-                return passt.tools.failed(target);
-            if(
-                (max > 0) &&
-                (checkStr.length > max)
-            )
-                return passt.tools.failed(target, 1);
-            return true;
-        }/*,
-        duplication:function(checkStr){
-            checkStr=[...new Set(checkStr)];
-
-        }*/
-    };
-    this.check=function(password){
-        passt.password = password;
-        passt.reset();
-        if(passt.setup.check.upperCase)
-            passt.log.checks.upperCase = passt.tools.check(
-                passt.password.match(/[A-Z]/g),
-                passt.setup.min.upperCase,
-                passt.setup.max.upperCase,
-                "upperCase"
-            );
-        if(passt.setup.check.lowerCase)
-            passt.log.checks.lowerCase = passt.tools.check(
-                passt.password.match(/[a-z]/g),
-                passt.setup.min.lowerCase,
-                passt.setup.max.lowerCase,
-                "lowerCase"
-            );
-        if(passt.setup.check.number)
-            passt.log.checks.number = passt.tools.check(
-                passt.password.match(/\d/g),
-                passt.setup.min.number,
-                passt.setup.max.number,
-                "number"
-            );
-        if(passt.setup.check.special)
-            passt.log.checks.special = passt.tools.check(
-                passt.password.match(/[$()@.!%*#?&]/g),
-                passt.setup.min.special,
-                passt.setup.max.special,
-                "special"
-            );
-        if(passt.setup.check.size)
-            passt.log.checks.size = passt.tools.check(
-                passt.password,
-                passt.setup.min.size,
-                passt.setup.max.size,
-                "size"
-            );
-        return passt.log.result;
-
-    };
-    var passt = this;
+    /*
+     * @private
+     * boolean
+     */
+    let checkDuplication = function(checkStr){
+        checkStr=[...new Set(checkStr)];
+    }
 };
 
 
